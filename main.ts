f955e371b64a154ccecfd642183311eaf5e517ba
2C24HaydenS <115450543+2C24HaydenS@users.noreply.github.com>
2022-11-12 22:04:12 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("" + (input.lightLevel()))
 })
-basic.showString("24")
+basic.forever(function () {
+    led.setBrightness(0)
+    if (input.lightLevel() > led.brightness()) {
+        led.setBrightness(input.lightLevel())
+    }
+    basic.showString("" + (led.brightness()))
+})
